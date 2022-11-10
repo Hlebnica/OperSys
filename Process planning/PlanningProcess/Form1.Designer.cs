6fd9445f33fe4b8162e7643fc7b04fad5bc4dff5
Дмитрий Старостенок <49001181+Hlebnica@users.noreply.github.com>
2022-11-10 14:18:11 +0500

    ProcessPlaning_in_work

--- Process planning/PlanningProcess/Form1.Designer.cs
+++ Process planning/PlanningProcess/Form1.Designer.cs
@@ -36,11 +36,21 @@
             this.AddProcessMiddle = new System.Windows.Forms.Button();
             this.CP_Counter_label = new System.Windows.Forms.Label();
             this.Memory_Counter_label = new System.Windows.Forms.Label();
+            this.CurrentTime_label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CPCustomProcess_textBox = new System.Windows.Forms.TextBox();
+            this.MemoryCustomProcess_textBox = new System.Windows.Forms.TextBox();
+            this.AddNewCustomProcess_button = new System.Windows.Forms.Button();
+            this.FCFS_groupBox = new System.Windows.Forms.GroupBox();
+            this.RR_groupBox = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.FCFS_groupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // CentralProcess
             // 
-            this.CentralProcess.Location = new System.Drawing.Point(85, 12);
+            this.CentralProcess.Location = new System.Drawing.Point(85, 66);
             this.CentralProcess.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.CentralProcess.Name = "CentralProcess";
             this.CentralProcess.Size = new System.Drawing.Size(38, 22);
@@ -50,7 +60,7 @@
             // 
             // Memory
             // 
-            this.Memory.Location = new System.Drawing.Point(85, 45);
+            this.Memory.Location = new System.Drawing.Point(85, 99);
             this.Memory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Memory.Name = "Memory";
             this.Memory.Size = new System.Drawing.Size(64, 20);
@@ -64,13 +74,13 @@
             this.ProcessList.Location = new System.Drawing.Point(264, 10);
             this.ProcessList.Margin = new System.Windows.Forms.Padding(4);
             this.ProcessList.Name = "ProcessList";
-            this.ProcessList.Size = new System.Drawing.Size(727, 388);
+            this.ProcessList.Size = new System.Drawing.Size(543, 388);
             this.ProcessList.TabIndex = 2;
             this.ProcessList.SelectedIndexChanged += new System.EventHandler(this.ProcessList_SelectedIndexChanged);
             // 
             // AddProcessLow
             // 
-            this.AddProcessLow.Location = new System.Drawing.Point(13, 487);
+            this.AddProcessLow.Location = new System.Drawing.Point(7, 22);
             this.AddProcessLow.Margin = new System.Windows.Forms.Padding(4);
             this.AddProcessLow.Name = "AddProcessLow";
             this.AddProcessLow.Size = new System.Drawing.Size(195, 50);
@@ -81,7 +91,7 @@
             // 
             // AddProcessMiddle
             // 
-            this.AddProcessMiddle.Location = new System.Drawing.Point(14, 560);
+            this.AddProcessMiddle.Location = new System.Drawing.Point(8, 95);
             this.AddProcessMiddle.Margin = new System.Windows.Forms.Padding(4);
             this.AddProcessMiddle.Name = "AddProcessMiddle";
             this.AddProcessMiddle.Size = new System.Drawing.Size(192, 53);
@@ -92,7 +102,7 @@
             // 
             // CP_Counter_label
             // 
-            this.CP_Counter_label.Location = new System.Drawing.Point(14, 10);
+            this.CP_Counter_label.Location = new System.Drawing.Point(14, 64);
             this.CP_Counter_label.Name = "CP_Counter_label";
             this.CP_Counter_label.Size = new System.Drawing.Size(64, 21);
             this.CP_Counter_label.TabIndex = 5;
@@ -101,7 +111,7 @@
             // 
             // Memory_Counter_label
             // 
-            this.Memory_Counter_label.Location = new System.Drawing.Point(14, 45);
+            this.Memory_Counter_label.Location = new System.Drawing.Point(14, 99);
             this.Memory_Counter_label.Name = "Memory_Counter_label";
             this.Memory_Counter_label.Size = new System.Drawing.Size(64, 21);
             this.Memory_Counter_label.TabIndex = 6;
@@ -109,15 +119,106 @@
             this.Memory_Counter_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.Memory_Counter_label.Click += new System.EventHandler(this.Memory_Counter_label_Click);
             // 
+            // CurrentTime_label
+            // 
+            this.CurrentTime_label.Location = new System.Drawing.Point(14, 37);
+            this.CurrentTime_label.Name = "CurrentTime_label";
+            this.CurrentTime_label.Size = new System.Drawing.Size(147, 23);
+            this.CurrentTime_label.TabIndex = 7;
+            this.CurrentTime_label.Text = "-";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 23);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Системное время:";
+            // 
+            // CPCustomProcess_textBox
+            // 
+            this.CPCustomProcess_textBox.Location = new System.Drawing.Point(8, 177);
+            this.CPCustomProcess_textBox.Name = "CPCustomProcess_textBox";
+            this.CPCustomProcess_textBox.Size = new System.Drawing.Size(112, 22);
+            this.CPCustomProcess_textBox.TabIndex = 9;
+            // 
+            // MemoryCustomProcess_textBox
+            // 
+            this.MemoryCustomProcess_textBox.Location = new System.Drawing.Point(7, 216);
+            this.MemoryCustomProcess_textBox.Name = "MemoryCustomProcess_textBox";
+            this.MemoryCustomProcess_textBox.Size = new System.Drawing.Size(113, 22);
+            this.MemoryCustomProcess_textBox.TabIndex = 10;
+            // 
+            // AddNewCustomProcess_button
+            // 
+            this.AddNewCustomProcess_button.Location = new System.Drawing.Point(133, 177);
+            this.AddNewCustomProcess_button.Name = "AddNewCustomProcess_button";
+            this.AddNewCustomProcess_button.Size = new System.Drawing.Size(69, 61);
+            this.AddNewCustomProcess_button.TabIndex = 11;
+            this.AddNewCustomProcess_button.Text = "Add Custom";
+            this.AddNewCustomProcess_button.UseVisualStyleBackColor = true;
+            this.AddNewCustomProcess_button.Click += new System.EventHandler(this.AddNewCustomProcess_button_Click);
+            // 
+            // FCFS_groupBox
+            // 
+            this.FCFS_groupBox.Controls.Add(this.AddProcessLow);
+            this.FCFS_groupBox.Controls.Add(this.AddNewCustomProcess_button);
+            this.FCFS_groupBox.Controls.Add(this.AddProcessMiddle);
+            this.FCFS_groupBox.Controls.Add(this.MemoryCustomProcess_textBox);
+            this.FCFS_groupBox.Controls.Add(this.CPCustomProcess_textBox);
+            this.FCFS_groupBox.Location = new System.Drawing.Point(25, 449);
+            this.FCFS_groupBox.Name = "FCFS_groupBox";
+            this.FCFS_groupBox.Size = new System.Drawing.Size(221, 256);
+            this.FCFS_groupBox.TabIndex = 12;
+            this.FCFS_groupBox.TabStop = false;
+            this.FCFS_groupBox.Text = "FCFS";
+            this.FCFS_groupBox.Visible = false;
+            // 
+            // RR_groupBox
+            // 
+            this.RR_groupBox.Location = new System.Drawing.Point(264, 449);
+            this.RR_groupBox.Name = "RR_groupBox";
+            this.RR_groupBox.Size = new System.Drawing.Size(266, 256);
+            this.RR_groupBox.TabIndex = 13;
+            this.RR_groupBox.TabStop = false;
+            this.RR_groupBox.Text = "RR";
+            this.RR_groupBox.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Location = new System.Drawing.Point(33, 161);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 33);
+            this.radioButton1.TabIndex = 14;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "FCFS";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Location = new System.Drawing.Point(33, 200);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 33);
+            this.radioButton2.TabIndex = 15;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "RR";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1004, 624);
+            this.ClientSize = new System.Drawing.Size(831, 751);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.RR_groupBox);
+            this.Controls.Add(this.FCFS_groupBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CurrentTime_label);
             this.Controls.Add(this.Memory_Counter_label);
             this.Controls.Add(this.CP_Counter_label);
-            this.Controls.Add(this.AddProcessMiddle);
-            this.Controls.Add(this.AddProcessLow);
             this.Controls.Add(this.ProcessList);
             this.Controls.Add(this.Memory);
             this.Controls.Add(this.CentralProcess);
@@ -125,8 +226,26 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.FCFS_groupBox.ResumeLayout(false);
+            this.FCFS_groupBox.PerformLayout();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.RadioButton radioButton2;
+
+        private System.Windows.Forms.RadioButton radioButton1;
+
+        private System.Windows.Forms.GroupBox RR_groupBox;
+
+        private System.Windows.Forms.GroupBox FCFS_groupBox;
+
+        private System.Windows.Forms.TextBox CPCustomProcess_textBox;
+        private System.Windows.Forms.TextBox MemoryCustomProcess_textBox;
+        private System.Windows.Forms.Button AddNewCustomProcess_button;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Label CurrentTime_label;
 
         private System.Windows.Forms.Label CP_Counter_label;
         private System.Windows.Forms.Label Memory_Counter_label;
--- Process planning/PlanningProcess/Form1.cs
+++ Process planning/PlanningProcess/Form1.cs
@@ -18,7 +18,6 @@ namespace PlanningProcess
         public int HighlightingCentralProcessor { get; set; } // нагрузка на процессор
         public int AllocatedMemory { get; set; } // Выделение памяти
         public DateTime AppearanceTime { get; set; } // Время появления
-        
         public int TimeToExecute { get; set; } // Время на выполнение задачи
 
         public Process(int id, int highlightingCentralProcessor, int allocatedMemory, int timeToExecute)
@@ -37,7 +36,6 @@ namespace PlanningProcess
     }
     
     
-    
     public partial class Form1 : Form
     {
         QueueFifo _process = new QueueFifo(); // Очередь для FIFO
@@ -50,7 +48,6 @@ namespace PlanningProcess
         {
             InitializeComponent();
             
-           
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -62,29 +59,37 @@ namespace PlanningProcess
                 {
                     if(ProcessList.Items.Count > 0) 
                     {
-                        Process itemNow = (Process) ProcessList.Items[0]; 
-                        Thread.Sleep(itemNow.TimeToExecute);
-                        label1.Text = itemNow.TimeToExecute.ToString();
+                        Process itemNow = (Process) ProcessList.Items[0]; // Получение текущей записи по ID 
+                        Thread.Sleep(itemNow.TimeToExecute); // Ожидание во время выполнения задачи
                         CentralProcessorCounter -= itemNow.HighlightingCentralProcessor;
                         CP_Counter_label.Text = CentralProcessorCounter.ToString();
                         AllocatedMemoryCounter -= itemNow.AllocatedMemory;
                         Memory_Counter_label.Text = AllocatedMemoryCounter.ToString();
-                        Invoke(new MethodInvoker(() => ProcessList.Items.RemoveAt(0)));
+                        Invoke(new MethodInvoker(() => ProcessList.Items.RemoveAt(0))); // Удаление из списка выполненной задачи
                     }
                 } 
             }).Start(); 
+            
+            // Показ системной даты 
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    CurrentTime_label.Text = DateTime.Now.ToString();
+                }   
+            }).Start();
         }
         
-        private void AddProcessLow_Click(object sender, EventArgs e)
+        private void AddProcessLow_Click(object sender, EventArgs e) // Добавление маленького процесса
         {
-            Process lowProcess = new Process(IdCounter, 10, 20, 2000);
-            _process.ProcessFifo.Enqueue(lowProcess);
-            ProcessList.Items.Add(_process.ProcessFifo.Dequeue());
-            IdCounter++;
-            CentralProcessorCounter += lowProcess.HighlightingCentralProcessor;
-            AllocatedMemoryCounter += lowProcess.AllocatedMemory;
-            CP_Counter_label.Text = CentralProcessorCounter.ToString();
-            Memory_Counter_label.Text = AllocatedMemoryCounter.ToString();
+            Process lowProcess = new Process(IdCounter, 10, 20, 2000); // Конструктор процесса
+            _process.ProcessFifo.Enqueue(lowProcess); // Добавление в очередь
+            ProcessList.Items.Add(_process.ProcessFifo.Dequeue()); // Добавить в список на форме
+            IdCounter++; // Увеличение ID для следующего процесса
+            CentralProcessorCounter += lowProcess.HighlightingCentralProcessor; // Добавление к счетчику процессора
+            AllocatedMemoryCounter += lowProcess.AllocatedMemory; // Добавление к счетчику памяти
+            CP_Counter_label.Text = CentralProcessorCounter.ToString(); // Запись значения счетчика процессора в label
+            Memory_Counter_label.Text = AllocatedMemoryCounter.ToString(); // Запись значения счетчика памяти в label
         }
 
         private void ProcessList_SelectedIndexChanged(object sender, EventArgs e)
@@ -92,7 +97,7 @@ namespace PlanningProcess
             
         }
 
-        private void AddProcessMiddle_Click(object sender, EventArgs e)
+        private void AddProcessMiddle_Click(object sender, EventArgs e) // Добавление процесса побольше
         {
             Process middleProcess = new Process(IdCounter, 15, 25, 5000);
             _process.ProcessFifo.Enqueue(middleProcess);
@@ -112,6 +117,35 @@ namespace PlanningProcess
         private void Memory_Counter_label_Click(object sender, EventArgs e)
         {
             
+        }
+
+        private void AddNewCustomProcess_button_Click(object sender, EventArgs e) // Добавление кастомного процесса
+        {
+            if(CPCustomProcess_textBox.Text != string.Empty && MemoryCustomProcess_textBox.Text != string.Empty)
+            {
+                Process customProcess = new Process(IdCounter, Convert.ToInt32(CPCustomProcess_textBox.Text),
+                    Convert.ToInt32(MemoryCustomProcess_textBox.Text), 
+                    Convert.ToInt32(CPCustomProcess_textBox.Text) * Convert.ToInt32(MemoryCustomProcess_textBox.Text) * 10);
+                _process.ProcessFifo.Enqueue(customProcess);
+                ProcessList.Items.Add(_process.ProcessFifo.Dequeue());
+                IdCounter++;
+                CentralProcessorCounter += customProcess.HighlightingCentralProcessor;
+                AllocatedMemoryCounter += customProcess.AllocatedMemory;
+                CP_Counter_label.Text = CentralProcessorCounter.ToString();
+                Memory_Counter_label.Text = AllocatedMemoryCounter.ToString();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            FCFS_groupBox.Visible = true;
+            RR_groupBox.Visible = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            FCFS_groupBox.Visible = false;
+            RR_groupBox.Visible = true;
         }
     }
 }
