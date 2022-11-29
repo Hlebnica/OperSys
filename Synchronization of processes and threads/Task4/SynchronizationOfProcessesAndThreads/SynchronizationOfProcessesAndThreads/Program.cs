54b7d118df531708dc0254c0e5fa9d869fc3e0e1
Дмитрий Старостенок <apple260500@gmail.com>
2022-11-30 00:58:12 +0500

    Task4

---
+++ Synchronization of processes and threads/Task4/SynchronizationOfProcessesAndThreads/SynchronizationOfProcessesAndThreads/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+List<int> buffer = new List<int>();
+int producer = 2000;
+int consumer = 2000;
+
+void Producer()
+{
+    while (true)
+    {
+        Random random = new Random();
+        if (buffer.Count <= 10)
+        {
+            buffer.Add(random.Next(15));
+        }
+        Thread.Sleep(producer);
+        foreach (var value in buffer)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
+
+void Consumer()
+{
+    Thread.Sleep(consumer);
+}
+
+Thread producerThread = new Thread(Producer);
+producerThread.Start();
